ae6fc831bf7c2159db8fc6375ef92d0a26451a3b
cyan-wings <matthewyeow.yeow@gmail.com>
2025-01-17 20:08:58 +0700

    Rendered basic sphere with basic ray hits

--- Rays/main.cpp
+++ Rays/main.cpp
@@ -5,8 +5,25 @@
 #include "vec3.hpp"
 #include "ray.hpp"
 
+/*
+ Quadratic formula ax^2 + bx + c to get where x intersects the sphere
+ to bounce the rays off the sphere if it hits.
+*/
+bool hit_sphere(const point3& center, double radius, const ray& r)
+{
+	vec3 oc = center - r.origin();
+	auto a = dot(r.direction(), r.direction());
+	auto b = -2.0 * dot(r.direction(), oc);
+	auto c = dot(oc, oc) - radius*radius;
+	auto discriminant = b*b - 4*a*c;
+	return (discriminant >= 0);
+}
+
 color ray_color( ray const &r )
 {
+	if (hit_sphere(point3(0,0,-1), 0.5, r))
+		return color(1, 0, 0);
+	
 	vec3	unit_direction = unit_vector(r.direction());
 	auto	a = 0.5*(unit_direction.y() + 1.0);
 	return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
