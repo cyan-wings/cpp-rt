fc536c1280e523455fec6820641b9a5b9febbc35
cyan-wings <matthewyeow.yeow@gmail.com>
2025-01-18 23:30:15 +0700

    Completed anti-aliasing portion

--- 4_Diffusion/includes/color.hpp
+++ 4_Diffusion/includes/color.hpp
@@ -1,6 +1,7 @@
 #ifndef __COLOR_HPP__
 # define __COLOR_HPP__
 
+# include "interval.hpp"
 # include "vec3.hpp"
 
 using color = vec3;
@@ -11,9 +12,10 @@ void write_color( std::ostream& out, color const &pixel_color )
 	auto	g = pixel_color.y();
 	auto	b = pixel_color.z();
 
-	int		rbyte = int(255.999 * r);
-	int		gbyte = int(255.999 * g);
-	int		bbyte = int(255.999 * b);
+	static	const interval intensity(0.000, 0.999);
+	int		rbyte = int(256 * intensity.clamp(r));
+	int		gbyte = int(256 * intensity.clamp(g));
+	int		bbyte = int(256 * intensity.clamp(b));
 
 	out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
 }
