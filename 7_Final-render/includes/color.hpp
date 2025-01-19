cb937091dfa96c25ad179253369dd47e0e5f6f88
cyan-wings <matthewyeow.yeow@gmail.com>
2025-01-20 00:43:28 +0700

    Completed final render

---
+++ 7_Final-render/includes/color.hpp
@@ -0,0 +1,36 @@
+#ifndef __COLOR_HPP__
+# define __COLOR_HPP__
+
+# include "interval.hpp"
+# include "vec3.hpp"
+
+using color = vec3;
+
+inline double linear_to_gamma(double linear_component)
+{
+	if (linear_component > 0)
+		return std::sqrt(linear_component);
+
+	return 0;
+}
+
+void write_color( std::ostream& out, color const &pixel_color )
+{
+	auto	r = pixel_color.x();
+	auto	g = pixel_color.y();
+	auto	b = pixel_color.z();
+
+	// Apply a linear to gamma transform for gamma 2
+	r = linear_to_gamma(r);
+	g = linear_to_gamma(g);
+	b = linear_to_gamma(b);
+
+	static	const interval intensity(0.000, 0.999);
+	int		rbyte = int(256 * intensity.clamp(r));
+	int		gbyte = int(256 * intensity.clamp(g));
+	int		bbyte = int(256 * intensity.clamp(b));
+
+	out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+}
+
+#endif
---
+++ 7_Final-render/includes/sphere.hpp
@@ -0,0 +1,55 @@
+#ifndef __SPHERE_HPP__
+# define __SPHERE_HPP__
+
+# include "hittable.hpp"
+# include "cpp_rt.hpp"
+
+class sphere : public hittable
+{
+	public:
+		sphere(const point3& center, double radius, shared_ptr<material> mat)
+		: center(center), radius(std::fmax(0,radius)), mat(mat) {}
+
+		/*
+		 Simplified to make computations faster.
+		 b = -2h , which removes the -2.0 factor in b
+		 and 4 factor in discriminant
+		*/
+		bool hit(const ray& r, interval ray_t, hit_record& rec) const override
+		{
+			vec3 oc = center - r.origin();
+			auto a = r.direction().length_squared();
+			auto h = dot(r.direction(), oc);
+			auto c = oc.length_squared() - radius*radius;
+
+			auto discriminant = h*h - a*c;
+			if (discriminant < 0)
+				return false;
+
+			auto sqrtd = std::sqrt(discriminant);
+
+			// Find the nearest root that lies in the acceptable range.
+			auto root = (h - sqrtd) / a;
+			if (!ray_t.surrounds(root))
+			{
+				root = (h + sqrtd) / a;
+				if (!ray_t.surrounds(root))
+					return false;
+			}
+
+			rec.t = root;
+			rec.p = r.at(rec.t);
+			vec3 outward_normal = (rec.p - center) / radius;
+			rec.set_face_normal(r, outward_normal);
+			rec.mat = mat;
+
+			return true;
+		}
+
+	private:
+		point3 center;
+		double radius;
+		shared_ptr<material> mat;
+};
+
+#endif
