85c890d21f14289b3351ce78b9d4cfe9b936060f
cyan-wings <matthewyeow.yeow@gmail.com>
2025-01-19 23:52:41 +0700

    Refactored camera viewport height to be adjustable later

---
+++ 6_Positionable-camera/main.cpp
@@ -0,0 +1,31 @@
+#include "cpp_rt.hpp"
+#include "camera.hpp"
+#include "hittable.hpp"
+#include "hittable_list.hpp"
+#include "material.hpp"
+#include "sphere.hpp"
+
+int	main( void )
+{
+	hittable_list	world;
+
+	auto R = std::cos(pi/4);
+
+	auto material_left  = make_shared<lambertian>(color(0,0,1));
+	auto material_right = make_shared<lambertian>(color(1,0,0));
+
+	world.add(make_shared<sphere>(point3(-R, 0, -1), R, material_left));
+	world.add(make_shared<sphere>(point3( R, 0, -1), R, material_right));
+
+	camera			cam;
+
+	cam.aspect_ratio = 16.0 / 9.0;
+	cam.image_width = 400;
+	cam.samples_per_pixel = 100;
+	cam.max_depth = 50;
+
+	cam.vfov = 90;
+
+	cam.render( world );
+	return ( 0 );
+}
